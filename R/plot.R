39a8c7027ed9bf98fba02064b333b38a4956b082
Nicolas Luyckx <nicolas.luyckx@sgs.com>
2018-11-05 18:03:01 +0100

    Remove se argument from plotting functions.

--- R/plot.R
+++ R/plot.R
@@ -6,7 +6,6 @@
 #' @param se.fit add a curve for the confidence interval around the fit
 #' @param mc.maxpts maximum number of points to use for the monte carlo fit
 #' @param .progress either "none" or "text" to see calculation progress of monte carlo simulations
-#' @param se add residual error bounds, currently not compatible with se.fit
 #' @param ... ignored
 #'
 #' @return a ggplot object with the fitted individual curve, the 95% ci of this curve, the population prediction, the observed data and the residual error around the observed data
@@ -14,7 +13,7 @@
 #' @importFrom ggplot2 ggplot aes_string geom_line geom_ribbon geom_point geom_errorbar labs
 #' @importFrom stats predict
 #' @export
-plot.tdmorefit <- function(x, newdata=NULL, se.fit=TRUE, mc.maxpts=100, .progress="none", se=FALSE, ...) {
+plot.tdmorefit <- function(x, newdata=NULL, se.fit=TRUE, mc.maxpts=100, .progress="none", ...) {
   tdmorefit <- x
   newdata <- processNewData(newdata, tdmorefit)
   args <- list(...)
@@ -22,31 +21,31 @@ plot.tdmorefit <- function(x, newdata=NULL, se.fit=TRUE, mc.maxpts=100, .progres
   population <- if (length(populationArg)==0) F else populationArg
 
   ipred <- tdmorefit %>% predict(newdata) %>% meltPredictions()
-  if(se.fit) {
+  if (se.fit) {
     ipredre <- tdmorefit %>% predict.tdmorefit(newdata, se.fit=T, level=0.95, mc.maxpts = mc.maxpts, .progress=.progress) %>% meltPredictions(se=T)
-  }
-  if(se) {
-    ipredre <- tdmorefit$tdmore %>% predict(newdata, tdmorefit$regimen, se=T) %>% meltPredictions(se=T)
   }
   yVars <- colnames(newdata)[colnames(newdata) != "TIME"]
 
   if (population) {
     # No need to compute PRED because IPRED = PRED
     plot <- ggplot(mapping=aes_string(x="TIME", y="value", group="variable")) + geom_line(color=blue(), data=ipred)
-    if(se.fit || se) plot <- plot + geom_ribbon(fill=blue(), aes_string(ymin="value.lower", ymax="value.upper"), data=ipredre, alpha=0.05)
+    if (se.fit) plot <- plot + geom_ribbon(fill=blue(), aes_string(ymin="value.lower", ymax="value.upper"), data=ipredre, alpha=0.05)
 
   } else {
     # Compute PRED
     pred <- estimate(tdmorefit$tdmore, regimen=tdmorefit$regimen, covariates=tdmorefit$covariates) %>% predict(newdata) %>% meltPredictions()
-    if(se.fit) predre <- estimate(tdmorefit$tdmore, regimen=tdmorefit$regimen, covariates=tdmorefit$covariates) %>% predict(newdata, se.fit=T) %>% meltPredictions(se=T)
+    if (se.fit) {
+      predre <- estimate(tdmorefit$tdmore, regimen=tdmorefit$regimen, covariates=tdmorefit$covariates) %>% predict(newdata, se.fit=T) %>% meltPredictions(se=T)
+    }
+
     obs <- model.frame.tdmorefit(tdmorefit) %>% meltPredictions()
     obs <- subset(obs, variable %in% yVars & !is.na(value))
 
     plot <- ggplot(mapping=aes_string(x="TIME", y="value", group="variable"))
     plot <- plot + geom_line(color=blue(), data=pred) + geom_point(data=obs)
     plot <- plot + geom_line(color=red(), data=ipred) + geom_point(data=obs)
-    if(se.fit) plot <- plot + geom_ribbon(fill=blue(), aes_string(ymin="value.lower", ymax="value.upper"), data=predre, alpha=0.05)
-    if(se.fit) plot <- plot + geom_ribbon(fill=red(), aes_string(ymin="value.lower", ymax="value.upper"), data=ipredre, alpha=0.04)
+    if (se.fit) plot <- plot + geom_ribbon(fill=blue(), aes_string(ymin="value.lower", ymax="value.upper"), data=predre, alpha=0.05)
+    if (se.fit) plot <- plot + geom_ribbon(fill=red(), aes_string(ymin="value.lower", ymax="value.upper"), data=ipredre, alpha=0.04)
   }
 
   plot <- plot + labs(y=paste(yVars, collapse = " / "))
@@ -62,7 +61,6 @@ plot.tdmorefit <- function(x, newdata=NULL, se.fit=TRUE, mc.maxpts=100, .progres
 #' @param bsv add between subject variability
 #' @param mc.subjects number of subjects in the monte carlo simulation
 #' @param .progress either "none" or "text" to see calculation progress of monte carlo simulations
-#' @param se add residual error bounds, currently not compatible with bsv
 #' @param ... ignored
 #'
 #' @return a ggplot object with the fitted individual curve, the 95% ci of this curve, the population prediction, the observed data and the residual error around the observed data
@@ -70,10 +68,10 @@ plot.tdmorefit <- function(x, newdata=NULL, se.fit=TRUE, mc.maxpts=100, .progres
 #' @importFrom ggplot2 ggplot aes_string geom_line geom_ribbon geom_point geom_errorbar labs
 #' @importFrom stats predict
 #' @export
-plot.tdmore <- function(x, regimen, covariates=NULL, newdata=NULL, bsv=TRUE, mc.subjects=100, .progress="none", se=FALSE, ...) {
+plot.tdmore <- function(x, regimen, covariates=NULL, newdata=NULL, bsv=TRUE, mc.subjects=100, .progress="none", ...) {
   tdmore <- x
   tdmorefit <- estimate(tdmore = tdmore, regimen = regimen, covariates = covariates)
-  return(plot(tdmorefit, newdata=newdata, se.fit=bsv, mc.maxpts=mc.subjects, .progress=.progress, se=se, population=TRUE))
+  return(plot(tdmorefit, newdata=newdata, se.fit=bsv, mc.maxpts=mc.subjects, .progress=.progress, population=TRUE))
 }
 
 processNewData <- function(newdata, tdmorefit) {
