781a1f0ee1cc7a528b33d934948514ce36bfe154
rfaelens <ruben.faelens@sgs.com>
2018-11-06 16:59:33 +0100

    Update of documentation

---
+++ R/tdmore-package.R
@@ -0,0 +1,9 @@
+#' TDMore: Estimate individual parameters for population pharmacometrics models, and use them to make predictions and/or dose recommendations.
+#'
+#' @name tdmore-package
+#' @importFrom magrittr %>%
+#' @docType package
+NULL
+
+# We need to import generic functions package-wide. See http://r-pkgs.had.co.nz/namespace.html#export-s3
+# > A method for a generic in a required package. You’ll need to import the generic (see below), and export the method.
