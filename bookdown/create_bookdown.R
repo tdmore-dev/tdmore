964d94b07fa85e9fb2a3ed5f82f8e4c1aa2fb305
rfaelens <ruben.faelens@kuleuven.be>
2018-11-14 11:42:50 +0100

    Optimize compiling bookdown book

--- bookdown/create_bookdown.R
+++ bookdown/create_bookdown.R
@@ -1,3 +1,4 @@
 setwd(devtools::package_file("bookdown/"))
 output <- devtools::package_file("docs/book/")
 bookdown::render_book(input="index.Rmd", output_format="bookdown::gitbook", output_dir = output)
+setwd(devtools::package_file())
