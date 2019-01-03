67255672002afe9c3df5a40bc2841a9b278caf78
rfaelens <ruben.faelens@kuleuven.be>
2019-01-03 14:40:27 +0100

    Adapt create_bookdown to allow execution in commit hook

--- bookdown/create_bookdown.R
+++ bookdown/create_bookdown.R
@@ -1,9 +1,17 @@
 setwd(devtools::package_file("bookdown/"))
-output <- devtools::package_file("docs/book/")
-bookdown::render_book(input="index.Rmd",
+bookoutputdir <- devtools::package_file("docs/book/")
+bookoutputdir <- force(bookoutputdir)
+message("Creating bookdown in ", bookoutputdir)
+
+## Because bookdown::render_book is executed in a new session, it does not have access to
+## variables defined in the current R script. This is why we need to use do.call(),
+## and cannot call bookdown::render_book directly (it would otherwise complain that 'bookoutputdir'
+## is an unknown variable )
+arguments <- list(input="index.Rmd",
                       output_format="bookdown::gitbook",
-                      output_dir = output,
+                      output_dir = bookoutputdir,
                       new_session=TRUE)
+do.call(bookdown::render_book, arguments)
 unlink("_bookdown_files", recursive=TRUE) #clean up
 unlink("_main.rds")
 setwd(devtools::package_file("."))
