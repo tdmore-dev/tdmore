4b1df88178e0747a08ebcf6abdea99e263e61948
rfaelens <ruben.faelens@kuleuven.be>
2019-09-13 14:16:46 +0200

    Further development on proseval functions

--- R/proseval.R
+++ R/proseval.R
@@ -4,7 +4,7 @@
 #' Each column from the tbl will be mapped to the `estimate()` function.
 #' You can generate such a tbl using \link{data} functions.
 #' @param ... additional arguments to add to each `estimate` call. If an argument
-#' is present both here and in .data, the argument here takes precedence.
+#' is present both here and in x, the argument here takes precedence.
 #' @param .fit Either a string or NULL. If a string, the output will contain a column
 #' with that name, storing the fit
 #' @param .prediction Either a string or NULL. If a string, the output will contain a column
@@ -16,23 +16,26 @@
 #'
 #' @export
 #' @examples
-#' data <- dataTibble(object=meropenem_model %>% tdmore(), observed=data.frame(ID=c(1,1,2,2), TIME=c(8,10,8,10), CONC=c(1.5,0.3,0.8,0.2)))
+#' require(magrittr)
+#' require(ggplot2)
+#' data <- dataTibble(
+#'     object=meropenem_model %>% tdmore(),
+#'     observed=data.frame(ID=c(1,1,2,2), TIME=c(8,10,8,10), CONC=c(1.5,0.3,0.8,0.2))
+#' )
 #' fit <- data %>%
 #'   posthoc(regimen=data.frame(TIME=0, AMT=1000))
-#' cluster <- multidplyr::new_cluster(2)
-#' data %>% group_by(ID) %>% partition(cluster) %>% posthoc(regimen=data.frame(TIME=0, AMT=1000))
 #' fit %>% tidyr::unnest(observed, ipred, .sep=".") %>% ggplot(aes(x=ipred.TIME)) +
-#' geom_point(aes(y=observed.CONC)) +
-#' geom_line(aes(y=ipred.CONC, group=ID)) +
-#' facet_wrap(~ID)
-posthoc <- function(.data, ..., .fit="fit", .prediction="ipred", .elapsed="elapsed", .id="ID") {
-  if(!setequal(class(.data), "list")) {
-    res <- dplyr::rowwise(.data) %>%
-      dplyr::do({posthoc(.data, ..., .fit=.fit, .prediction=.prediction, .elapsed=.elapsed, .id=.id)})
+#'   geom_point(aes(y=observed.CONC)) +
+#'   geom_line(aes(y=ipred.CONC, group=ID)) +
+#'   facet_wrap(~ID)
+posthoc <- function(x, ..., .fit="fit", .prediction="ipred", .elapsed="elapsed", .id="ID") {
+  if(!setequal(class(x), "list")) {
+    res <- x %>% dplyr::rowwise() %>%
+      dplyr::do({posthoc(., ..., .fit=.fit, .prediction=.prediction, .elapsed=.elapsed, .id=.id)})
     return(res)
   } else {
-    stopifnot(is.list( .data ))
-    args <- .data
+    stopifnot(is.list( x ))
+    args <-x
     args[.id] <- NULL
     args[names(list(...))] <- list(...) #add/override using common arguments
     tic <- proc.time()
@@ -42,7 +45,7 @@ posthoc <- function(.data, ..., .fit="fit", .prediction="ipred", .elapsed="elaps
     #You can try this yourself:
     #> foo <- list(bla1 = 1, bla2 = data.frame(TIME=3, CONC=2))
     #> as_tibble(foo)
-    res <- lapply(.data, function(x){if(rlang::is_atomic(x)) x else list(x)})
+    res <- lapply(x, function(x){if(rlang::is_atomic(x)) x else list(x)})
     if(!is.null(.fit)) res[[.fit]] <- list( fit )
     if(!is.null(.elapsed)) res[[.elapsed]] <- list( toc - tic )
     if(!is.null(.prediction)) res[[.prediction]] <- list( predict(fit) )
@@ -62,24 +65,24 @@ posthoc <- function(.data, ..., .fit="fit", .prediction="ipred", .elapsed="elaps
 #' And so on.
 #'
 #' @export
-proseval <- function(.data, ..., .fit="fit", .prediction="ipred", .elapsed="elapsed", .obs="OBS") {
+proseval <- function(x, ..., .fit="fit", .prediction="ipred", .elapsed="elapsed", .obs="OBS") {
   if(is.null(.fit)) stop(".fit column needs to be present for proseval to work")
-  if(tibble::is_tibble(.data)) {
-    res <- dplyr::rowwise(.data) %>%
-      dplyr::do({proseval(.data, ..., .fit=.fit, .prediction=.prediction, .elapsed=.elapsed, .obs=.obs)})
+  if(tibble::is_tibble(x)) {
+    res <- dplyr::rowwise(x) %>%
+      dplyr::do({proseval(., ..., .fit=.fit, .prediction=.prediction, .elapsed=.elapsed, .obs=.obs)})
     return(res)
   } else {
-    stopifnot(is.list( .data ))
+    stopifnot(is.list( x ))
 
     output <- list()
-    for(i in c(0, seq_len(nrow(.data$observed))) ){
-      argsPosthoc <- .data
+    for(i in c(0, seq_len(nrow(x$observed))) ){
+      argsPosthoc <- x
       if(i == 0) argsPosthoc['observed'] <- list(NULL)
-      else argsPosthoc$observed <- .data$observed[ seq_len(i), ]
+      else argsPosthoc$observed <- x$observed[ seq_len(i), ]
       res <- posthoc(argsPosthoc, ..., .fit=.fit, .prediction=NULL, .elapsed=.elapsed)
       fit <- res[[.fit]][[1]]
-      res[["observed"]] <- list(.data$observed) #always use the same OBSERVED data.frame
-      if(!is.null(.prediction)) res[[.prediction]] <- list( predict(fit, .data$observed) )
+      res[["observed"]] <- list(x$observed) #always use the same OBSERVED data.frame
+      if(!is.null(.prediction)) res[[.prediction]] <- list( predict(fit, x$observed) )
       if(!is.null(.obs)) res[[.obs]] <- i
       output[[i+1]] <- res
     }
@@ -93,6 +96,10 @@ proseval <- function(.data, ..., .fit="fit", .prediction="ipred", .elapsed="elap
 #' @param predict a prediction function for dose simulation, corresponding to function(truth, newRegimen, nextTime)
 #' If missing, we simply predict using the true fit.
 #' We evaluate the nextObservations time points using the 'true' model and the new regimen.
+#' @param .iterationFit column name for the iteration fit
+#' @param .next_observed What will be the predicted next observation?
+#' @param .next_regimen What will be the next regimen?
+#' @param .verbose Be verbose when simulating?
 #'
 #' @section DoseSimulation:
 #' This tool simulates a dose adaptation run for an individual.
@@ -108,41 +115,43 @@ proseval <- function(.data, ..., .fit="fit", .prediction="ipred", .elapsed="elap
 #'   It is up to the optimizer to determine how to optimize the treatment regimen.
 #'   The optimizer returns a list( nextTime=nextObservations, regimen=newRegimen, extra=tibble() )
 #'   If nextTime is not a finite number, we stop the simulation.
+#'   If nextTime is missing, we use the next observation from the posthocfit.
 #' 3) The predict method is called with the following arguments:
 #'   predict(truth, newRegimen, nextTime)
 #' This predicts the next observation.
 #'
 #'
 #' @export
-doseSimulation <- function(.data, ..., optimize, predict,
+doseSimulation <- function(x, ..., optimize, predict,
                            .fit="fit", .iterationFit="iterationFit", .next_observed="next_observed", .next_regimen="next_regimen",
                            .verbose=TRUE) {
-  if(! .fit %in% names(.data)) stop(".fit column needs to be present in .data for doseSimulation to work")
+  if(! .fit %in% names(x)) stop(".fit column needs to be present in x for doseSimulation to work")
   if(missing(predict)) predict <- function(truth, regimen, time) {
     stats::predict(truth, regimen=regimen, newdata=time)
   }
-  if(tibble::is_tibble(.data)) {
-    res <- dplyr::rowwise(.data) %>%
-      dplyr::do({doseSimulation(.data, ..., optimize=optimize, predict=predict,
+  if(tibble::is_tibble(x)) {
+    res <- dplyr::rowwise(x) %>%
+      dplyr::do({doseSimulation(., ..., optimize=optimize, predict=predict,
                                 .fit=.fit, .iterationFit=.iterationFit, .next_observed=.next_observed, .next_regimen=.next_regimen)})
     return(res)
   } else {
-    stopifnot(is.list( .data ))
+    stopifnot(is.list( x ))
     output <- list()
 
-    truth <- .data[[.fit]]
+    truth <- x[[.fit]]
     regimen <- truth$regimen
     observed <- tibble::tibble(TIME=numeric()) ## specifying NULL will reuse the tdmorefit observed values!!
     repeat {
       if(.verbose) cat("Iteration #", nrow(observed), "\n")
       # 1) a fit is generated using up-to-now observed concentrations
-      args <- .data
+      args <- x
       args[[.fit]] <- NULL
       args$observed <- observed
       args$regimen <- regimen
       res <- posthoc(args, ..., .fit=.iterationFit, .prediction=NULL, .elapsed=NULL)
 
       optimizeResult <- optimize(res[[.iterationFit]][[1]], regimen, truth)
+      if(is.null(optimizeResult$nextTime)) optimizeResult$nextTime <- truth$observed$TIME[ nrow(observed)+1 ]
 
       extra <- optimizeResult$extra
       if(!is.null(extra)) res[ , names(extra) ] <- extra
@@ -164,7 +173,7 @@ doseSimulation <- function(.data, ..., optimize, predict,
 
     outTibble <- bind_rows(output)
 
-    res <- lapply(.data, function(x){if(rlang::is_atomic(x)) x else list(x)})
+    res <- lapply(x, function(x){if(rlang::is_atomic(x)) x else list(x)})
     outTibble[, names(res)] <- res
 
     outTibble
