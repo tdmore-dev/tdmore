4d982710ac3ea1d5168195dcac1ec6125855775c
rfaelens <ruben.faelens@kuleuven.be>
2019-11-13 09:53:05 +0100

    Work in Progress for ggplot implementation
    This work in progress is comitted so I can find it back later.
    It is not an optimal solution, but contains some neat ideas.

--- R/ggplot.R
+++ R/ggplot.R
@@ -2,64 +2,8 @@
 ## We use fortify to convert the tdmorefit to a special data.frame
 ## It remembers all arguments passed to fortify.tdmorefit
 ##
-## Any stat_predict or other functions will generate a layer,
-## but the layer will have a special extra GGTdmoreLayer class.
-## When it is added to a plot using ggplot_add.GGTdmoreLayer,
-## we use the original plot data to fill in missing arguments for the layer stat methods.
-##
-## There is one potential bug:
-## If %+% is used, the new tdmorefit is not applied.
-## To avoid this, we mark the resulting plot from 'ggplot_add'
-## with a 'ggtdmore'
-## The %+%.ggtdmore function will always generate an error.
-
-#' @export
-#' @importFrom ggplot2 fortify
-fortify.tdmorefit <- function(model, newdata=NULL, regimen=NULL, covariates=NULL, parameters=NULL, ...) {
-  observed <- model.frame(model, data=newdata)
-  observed <- ggplot2::fortify(observed)
-  attr(observed, 'tdmoreArgs') <- list(
-    tdmorefit=model,
-    regimen=regimen,
-    covariates=covariates,
-    parameters=parameters,
-    extraArgs=list(...)
-    )
-  observed
-}
-has_tdmoreArgs <- function(x) {"tdmoreArgs" %in% names(attributes(x)) }
-
-`%||%` <- function(x,y) {if(!is.null(x)) x else y}
-
-#' This method extends ggplot2 to allow inheritance of tdmore arguments
-#' from the original `ggplot()` call into the specific tdmore-related layer.
-#'
-#' @inheritParams ggplot2::ggplot_add
-#' @keywords internal
-#' @importFrom ggplot2 ggplot_add
-#'
-#' @export
-ggplot_add.GGTdmoreLayer <- function(object, plot, object_name) {
-  class(object) <- setdiff(class(object), "GGTdmoreLayer") #unset class
-
-  data <- plot$data
-  layer <- object
-
-  #params in layer: geom_params, stat_params, aes_params,
-  list_union <- function(foo, bar){ c(foo, bar)[ union(names(foo), names(bar)) ] }
-  if(has_tdmoreArgs(data)) {
-    tdmoreArgs <- attr(data, 'tdmoreArgs')
-    for(i in intersect(names(tdmoreArgs), names(layer$stat_params))) {
-      value <- object$stat_params[[i]] %||% tdmoreArgs[[i]]
-      #beware, assigning NULL to a list element removes that element!
-      if(!is.null(value)) object$stat_params[[i]] <- value
-    }
-    object$stat_params$extraArgs <- tdmoreArgs$extraArgs
-  }
-
-  NextMethod()
-}
-
+## Any stat_predict or other functions will generate a layer with a special layer_class
+## This layer_class can change the data.frame to the predicted values instead
 
 #' Calculate predictions using the provided tdmore model. This is very similar to stat_function,
 #' but the calculated values are calculated using `predict.tdmorefit`.
@@ -81,9 +25,7 @@ ggplot_add.GGTdmoreLayer <- function(object, plot, object_name) {
 #'
 #' This data is used in the `predict()` call.
 #'
-#' @inheritParams predict.tdmorefit
 #' @inheritParams ggplot2::stat_function
-#' @param tdmorefit if specified, overrides the fit object
 #' @export
 #'
 #' @details
@@ -103,77 +45,31 @@ ggplot_add.GGTdmoreLayer <- function(object, plot, object_name) {
 #'     stat_predict(tdmorefit=pred, xlim=c(NA, NA))
 stat_predict <- function(mapping=NULL, data=NULL, geom="line",
                          position="identity",
-                         tdmorefit=NULL, regimen=NULL, parameters=NULL, covariates=NULL,
                          xlim=NULL, n=101,
                          se.fit=FALSE, level=0.95,
                          ...,
                          show.legend=NA, inherit.aes=TRUE) {
-  # ggplot complains if we provide `data=seq(0, 10)`, as that is not a data.frame
-  if(is.numeric(data)) {
-    data <- data.frame(TIME=data)
-  }
+  params <- list(
+    xlim=xlim,
+    n=n,
+    se.fit=se.fit, level=level,
+    ...
+  )
   z <- ggplot2::layer(
     data = data, mapping = mapping, stat = StatPredict,
     geom = geom, position = position, show.legend = show.legend,
     inherit.aes = inherit.aes,
-    params = list(
-      xlim=xlim,
-      n=n,
-      tdmorefit=tdmorefit,
-      regimen=regimen,
-      parameters=parameters,
-      covariates=covariates,
-      ...
-      )
+    params = params,
+    layer_class=TDMoreInheritLayer
   )
-  #allow override of layer parameters by ggtdmore object in ggplot_add.GGTdmoreLayer
-  class(z) <- union("GGTdmoreLayer", class(z))
   z
 }
 
-geom_fit <- function(mapping=NULL, data=NULL,
-                         tdmorefit=NULL, regimen=NULL, parameters=NULL, covariates=NULL,
-                         xlim=NULL, n=101,
-                         ...,
-                     color="red",
-                         show.legend=NA, inherit.aes=TRUE) {
-  stat_predict(mapping=mapping,
-               data=data,
-               geom="line",
-               color=color,
-               xlim=xlim,
-               n=n,
-               show.legend=show.legend,
-               inherit.aes=inherit.aes)
-}
-
-## Ggplot_build does the following with the 'data' argument before we get to compute_statistic:
-## layers <- plot$layers
-## layer_data <- lapply(layers, function(y) y$layer_data(plot$data))
-##    allow the layer to override `data` from the original plot, or filter using a function
-## layout <- create_layout(plot$facet, plot$coordinates)
-## data <- layout$setup(layer_data, plot$data, plot$plot_env)
-##    data <- c(list(plot_data), data)
-##    data <- self$facet$setup_data(data, self$facet_params)
-##      Split the data.frame for every panel, and add a PANEL column.
-##    data <- self$coord$setup_data(data, self$coord_params)
-##      Never actually used by any default coord- in ggplot
-## data <- by_layer(function(l, d) l$compute_aesthetics(d, plot))
-##    Create a new data.frame with the computed values from the aesthetics.
-##    If this is completely empty, or completely determined by stat(), then create a data.frame with length 1
-## data <- lapply(data, scales_transform_df, scales = scales)
-## layout$train_position(data, scale_x(), scale_y())
-## data <- layout$map_position(data)
-##    Execute the 'map' function on all x-y values
-## data <- by_layer(function(l, d) l$compute_statistic(d, layout))
-##    Finally, we compute the statistic
-##
-## We need to ensure that the data.frame is not empty at this point, otherwise
-## setup_data never gets called by compute_statistic!
 StatPredict <- ggplot2::ggproto("StatPredict", ggplot2::Stat,
-  #default_aes = ggplot2::aes(x=TIME), #Does not work! This is applied only _after_ stats are calculated
+  default_aes = ggplot2::aes(x=stat(TIME), y=stat(CONC)), #Does not work! This is applied only _after_ stats are calculated
   compute_group = function(data, scales,
-                           tdmorefit, regimen, parameters, covariates, extraArgs = list(),
+                           tdmorefit, regimen=NULL, parameters=NULL, covariates=NULL,
+                           se.fit=FALSE, level=0.95, mc.maxpts=100,
                            xlim = NULL, n = 101) {
     if(!is.null(xlim)) {
       # Evenly spaced grid, determined by xlim
@@ -206,18 +102,153 @@ StatPredict <- ggplot2::ggproto("StatPredict", ggplot2::Stat,
       c(list(object=tdmorefit,
            newdata=xseq,
            regimen=regimen,
-           covariates=covariates),
-        extraArgs)
+           parameters=parameters,
+           covariates=covariates,
+           se.fit=se.fit,
+           level=level,
+           mc.maxpts=mc.maxpts))
     )
 
     # Push new columns into `data`
     for(i in setdiff(names(result), names(data))) data[,i] <- result[,i]
 
+    # FIXME: if the aesthetics contained 'aes(y=CONC)', then it is a pity this is not maintained...
+
+
+    # FIXME
     # Assign 'y' as first variable with residual error
-    vars <- lapply(tdmorefit$tdmore$res_var, function(x) x$var)
-    if(length(vars) > 0 && !"y" %in% colnames(result))
-      data$y <- result[, vars[[1]] ] #first output column
+    #vars <- lapply(tdmorefit$tdmore$res_var, function(x) x$var)
+    #vars <- unname(vars)
+    #if(length(vars) > 0 && !"y" %in% colnames(result)) {
+    #  data$y <- result[, vars[[1]] ] #first output column
+    #}
 
     data
   }
 )
+
+## This layer takes care of inheriting the provided parameters of ggplot() into the arguments of geom and stat
+TDMoreInheritLayer <- ggplot2::ggproto(
+  "TDMoreInheritLayer",
+  ggplot2:::Layer,
+  setup_layer = function(self, data, plot) {
+    plotData <- plot$data
+    if(has_tdmoreArgs(plotData)) { ## if it is a tdmore data object, it can be used to override missing parameters
+      tdmoreArgs <- attr(plotData, 'tdmoreArgs')
+
+      # a layer has geom_params and stat_params
+      # we can nicely fill missing values using the tdmoreArgs
+      i <- intersect(names(tdmoreArgs), self$geom$parameters(extra=TRUE) )
+      self$geom_params[i] <- tdmoreArgs[i]
+
+      i <- intersect(names(tdmoreArgs), self$stat$parameters(extra=TRUE) )
+      self$stat_params[i] <- tdmoreArgs[i]
+    }
+
+    data
+  }
+)
+
+##### ----- The below code is experimental, and relies heavily on ggplot2 internals! -----
+## This layer replaces the data with a predicted data.frame
+## In layer_data, we set up the tdmoreArgs frame
+TDMorePredictLayer <- ggplot2::ggproto(
+  "TDMorePredictLayer",
+  ggplot2:::Layer,
+  tdmoreArgs=NULL, ## cache parameter
+  layer_data = function(self, plot_data) {
+    super <- ggproto_parent(ggplot2:::Layer, self)
+    layerData <- super$layer_data(plot_data)
+
+    ## First try self$data. Maybe it has some 'tdmoreArgs'?
+    params <- list()
+    if(has_tdmoreArgs(self$data)) {
+      tdmoreArgs <- attr(self$data, 'tdmoreArgs')
+      ## What is not yet specified comes from tdmoreArgs
+      i <- setdiff(names(tdmoreArgs), names(params))
+      params[i] <- tdmoreArgs[i]
+    } else if (!is.waive(self$data)) {
+      ### It is not a waiver; someone really specified something!
+      ### Use it as newdata argument
+      if(!empty(layerData) && is.null(params$newdata)) params$newdata <- layerData
+    }
+
+    ## Also check the plot_data!
+    if(has_tdmoreArgs(plot_data)) {
+      tdmoreArgs <- attr(plot_data, 'tdmoreArgs')
+      ## What is not yet specified comes from tdmoreArgs
+      i <- setdiff(names(tdmoreArgs), names(params))
+      params[i] <- tdmoreArgs[i]
+    }
+
+    if(is.null(params$object)) params$object <- params$tdmorefit #ensure the 'object' parameter is defined
+    self$tdmoreArgs <- params
+
+    do.call(what=predict, args=params) ## Already pass a prediction, even though it may be a prediction without observed data
+  },
+  setup_layer = function(self, data, plot) {
+    self$plot <- plot ## Store the plot for later use
+    data
+  },
+  compute_statistic = function(self, data, layout) {
+    ### Now calculate the data.frame
+    if(is.null(self$tdmoreArgs)) stop()
+    params <- self$tdmoreArgs
+    browser()
+
+    data <- plyr::ddply(data, "PANEL", function(data) {
+      if(is.null(params$newdata)) {
+        scales <- layout$get_scales(data$PANEL[1])
+        range <- range( c(scales$x$dimension(), layout$coord$limits$x) )
+        params$newdata <- seq(range[1], range[2], length.out=101)
+      }
+      result <- do.call(what=predict, args=params)
+      result$PANEL <- data$PANEL[1]
+
+      browser()
+      data <- self$compute_aesthetics(result, self$plot) ## re-compute previous aes
+      data <- scales_transform_df(scales, data)
+      data <- layout$map_position(data)
+
+      for(i in setdiff(names(result), names(data))) data[,i] <- result[,i] ## add missing columns
+
+      data
+    })
+
+    super <- ggproto_parent(ggplot2:::Layer, self)
+    data <- super$compute_statistic(data, layout)
+    data
+  }
+)
+
+#' A geom to display a fit object as a line
+#' @export
+geom_fit <- function(mapping=NULL, data=NULL,
+                     stat="identity",
+                     position="identity",
+                     ...,
+                     se.fit=FALSE, level=0.95,
+                     color="tomato1",
+                     show.legend=NA, inherit.aes=TRUE) {
+  ## Add the tdmoreArgs to the list
+  tdmoreArgs <- list(
+    se.fit=se.fit, level=level
+  )
+  data <- fortify(data)
+  attr(data, 'tdmoreArgs') <- c( tdmoreArgs, attr(data, 'tdmoreArgs'))
+
+  params <- list(
+    color=color,
+    ...
+  )
+
+  z <- ggplot2::layer(
+    data = data, mapping = mapping, stat = stat,
+    geom = "line", position = position, show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = params,
+    layer_class=TDMorePredictLayer
+  )
+  z
+}
+
