490b76c00d946534ea4e8e1e25934b0623e133a6
rfaelens <ruben.faelens@kuleuven.be>
2019-11-18 16:51:31 +0100

    Store benchmark results in helper file

--- tests/testthat/helperBenchmark.R
+++ tests/testthat/helperBenchmark.R
@@ -1,8 +1,16 @@
 library(benchmarkme)
 
-message("Running reference benchmark...")
-ref = benchmarkme::bm_prog_escoufier(runs = 1, verbose=FALSE)
-message("DONE. Reference elapsed time: ", ref$elapsed, " s")
+file <- file.path( system.file("benchmark", package="tdmore"),
+                       paste0(Sys.info()['nodename'], ".benchmark"))
+if(!file.exists(file)) {
+  message("Running reference benchmark...")
+  ref = benchmarkme::bm_prog_escoufier(runs = 1, verbose=FALSE)
+  message("DONE. 1 jiffie = ", ref$elapsed, " s")
+  saveRDS(ref, file)
+} else {
+  ref = readRDS(file)
+  message("Using cached benchmark result (1 jiffie = ", signif(ref$elapsed, 3), "s)...")
+}
 
 #'
 #' @param lower The runtime should be higher than `lower*reference`
@@ -10,6 +18,11 @@ message("DONE. Reference elapsed time: ", ref$elapsed, " s")
 expect_runtime <- function(expr, file, lower=0, upper=1.2) {
   res <- system.time(force(expr), gcFirst=TRUE)
   relTime <- res["elapsed"] / ref$elapsed #X jiffies
+
+  if(isTRUE(as.logical(Sys.getenv("R_COVR")))) {
+    rlang::warn("R_COVR is active, skipping runtime tests...")
+    return()
+  }
 
   if (!file.exists(file)) {
     warning("Creating reference value", call. = FALSE)
