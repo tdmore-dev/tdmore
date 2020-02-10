f320441cdd64c3d5f29eae412ec5b45e2467c1fb
rfaelens <ruben.faelens@kuleuven.be>
2020-02-10 16:53:27 +0100

    RxODE version change: vdiffr path not updated

--- tests/testthat/testProfile.R
+++ tests/testthat/testProfile.R
@@ -39,20 +39,20 @@ expect_warning({
 
 z <- z + facet_wrap(~EKa)
 #print(z) #Ka does not shift the LL-surface of ECL/EV1
-vdiffr::expect_doppelganger("profile_with_Ka_facet", z)
+expect_doppelganger("profile_with_Ka_facet", z)
 
 z <- plot(profile, parameters="EKa")
 #print(z) #Ka clearly does not matter
-vdiffr::expect_doppelganger("profile_on_Ka", z)
+expect_doppelganger("profile_on_Ka", z)
 
 # You can specify NA to estimate the parameter at every step
 profile <- profile(ipred, maxpts=20, fix=c(EKa=0, coef(ipred)[c("EV2", "EQ")]))
 z1 <- plot(profile, parameters="ECL") #LL-profile with EV1 jumping around
-vdiffr::expect_doppelganger("ecl_different_ev1", z1)
+expect_doppelganger("ecl_different_ev1", z1)
 
 profile <- profile(ipred, maxpts=20, fix=c(EV1=NA, EKa=0, coef(ipred)[c("EV2", "EQ")]))
 z2 <- plot(profile) #LL-profile with EV1 estimated
-vdiffr::expect_doppelganger("ecl_optimized_ev1", z2)
+expect_doppelganger("ecl_optimized_ev1", z2)
 
 #gridExtra::grid.arrange(z1 + coord_cartesian(xlim=c(-0.5, 0.5), ylim=c(0, 0.003)),
 #                        z2 + coord_cartesian(xlim=c(-0.5, 0.5), ylim=c(0, 0.003)))
@@ -67,4 +67,4 @@ expect_error(
 )
 
 profile <- profile(ipred, maxpts = 20, limits = list(ECL=c(-0.3,0.3), EV1=c(-0.8,0)), fix=coef(ipred)[c("EKa", "EV2", "EQ")])
-vdiffr::expect_doppelganger("profile_limits", plot(profile, contour=F))
+expect_doppelganger("profile_limits", plot(profile, contour=F))
